fa6e1b6ed24a6236695aba08062d42d54fef2357
Revo <ganandocriptomonedass@gmail.com>
2024-03-10 02:41:46 -0500

    pruebas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <div>
+      <q-carousel
+        animated
+        v-model="slide"
+        navigation
+        infinite
+        :autoplay="autoplay"
+        arrows
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        @mouseenter="autoplay = false"
+        @mouseleave="autoplay = true"
+      >
+        <q-carousel-slide :name="1" img-src="../assets/main.jpg" />
+        <q-carousel-slide :name="2" img-src="../assets/quees.jpg" />
+      </q-carousel>
+    </div>
+    <q-img
+    src="../assets/funciones.jpg"
+    :alt="altText"
+    basic
+    style="max-width: 100%; height: auto;"
+    class="rounded-borders"
+  />
+  <q-img
+    src="../assets/metricas.jpg"
+    :alt="altText"
+    basic
+    style="max-width: 100%; height: auto;"
+    class="rounded-borders"
+  />
+  <q-img
+    src="../assets/equipo.jpg"
+    :alt="altText"
+    basic
+    style="max-width: 100%; height: auto;"
+    class="rounded-borders"
+  />
+  </template>
+  <script setup>
+  import { ref } from 'vue'
+  
+  const slide = ref(1)
+  const autoplay = ref(true)
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  </script>
